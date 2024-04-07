49340aeddf9ad2432fc25266d9c5931fecbeac49
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-04-08 03:25:22 +0530

    MAIL SENDER

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+                from: 'Upskill Yourself || CodeHelp - by Azeem Khan',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,

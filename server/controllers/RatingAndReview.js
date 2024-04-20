bb4e11f1164332e009f47efa8bc9b5b79aa5f172
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-04-20 21:15:50 +0530

    Course Controller : "RatingAndReview" code with validation

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -69,4 +69,68 @@ exports.createRating = async(req,res)=>{
 
 //getAverageRating
 
-//getAllRating
+exports.getAverageRating = async(req, res) => {
+   //get courseId
+   const { courseId } = req.body;
+   //calculate average rating
+   const result = await RatingAndReviews.aggregate([
+    {
+        $match:{
+            courses : new mongoose.Types.ObjectId(courseId)
+        },
+    },
+    {
+        $group:{
+            _id: null,
+            averageRating:{
+                $avg: "$rating"
+            }
+        }
+    }
+   ]);
+
+   //return response
+   if(result.length > 0){
+      return res.status(200).json({
+          success : true,
+          message : 'Average rating is '+result[0].averageRating,
+          averageRating : result[0].averageRating
+      })
+   }
+   //if no rating/review exist
+   return res.status(200).json({
+        success : true,
+        message : 'Average rating is 0, no ratings are given till now.',
+        averageRating : 0
+   })
+}
+
+//getAllRatingAndReviews
+
+exports.getAllRating = async (req,res) => {
+ try {
+      const allReviews = await RatingAndReviews.find({})
+                                               .sort({rating: "desc"})
+                                               .populate({
+                                                   path: "User",
+                                                   select: "firstName lastName email image"
+                                               })
+                                               .populate({
+                                                  path: "Course",
+                                                  select: "courseName"
+                                               })
+                                               .exec();
+            return res.status(200).json({
+                success : true,
+                message : 'All reviews fetched successfully.',
+                data: allReviews
+            });                                   
+ } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      success : false,
+      message : 'Internal Server Error',
+      error : error.message,    
+    })
+ }
+}

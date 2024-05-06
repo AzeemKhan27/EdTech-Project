d28177408ba21cf6d6bf1e37ab4875d6255d7ae3
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-05-06 20:57:04 +0530

    Router : Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Navbar from "./components/common/Navbar.jsx"
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex-col flex font-inter">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} />

daefdd9c5ddf818e7cff075813850ce06ffdbb14
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-05-20 12:39:06 +0530

    Dashboard/MyProfile : Section 2 , section 3 all logics complete in Frontend

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -38,6 +38,60 @@ const MyProfile = () => {
             </IconBtn>
         </div>
 
+        {/* section-2 */}
+
+        <div>
+            <div>
+                <p>About</p>
+                <IconBtn 
+                    text="Edit"
+                    onClick={() => {
+                        navigate("/dashboard/settings")
+                    }} />
+            </div>
+            <p> { user?.additionalDetails?.about ?? "Write Somthing about yourself." } </p>
+        </div>
+
+        {/* section-3 */}
+
+        <div>
+            <div>
+                <p>Personal Details</p>
+                <IconBtn 
+                    text="Edit"
+                    onClick={() => {
+                        navigate("/dashboard/settings")
+                    }} />
+            </div>
+
+            <div>
+                <div>
+                    <p>First Name</p>
+                    <p>{user?.firstName}</p>
+                </div>
+                <div>
+                    <p>Email</p>
+                    <p>{user?.email}</p>
+                </div>
+                <div>
+                    <p>Gender</p>
+                    <p>{user?.additionalDetails?.gender ?? "Add Your Gender"}</p>
+                </div>
+                <div>
+                    <p>Last Name</p>
+                    <p>{user?.lastName}</p>
+                </div>
+                <div>
+                    <p>Phone Number</p>
+                    <p>{user?.additionalDetails?.contactNumber ?? "Add Your Contact Number"}</p>
+                </div>
+                <div>
+                    <p>Date of Birth</p>
+                    <p>{user?.additionalDetails?.dateOfBirth ?? "Add Your Date of Birth"}</p>
+                </div>
+            </div>
+        </div>
+
     </div>
   )
 }

d1e02f52f8b22ed9d8736c0ba3357264b447bf54
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-06-01 08:16:18 +0530

    Debug : correct Typo from backend and frontend

--- server/controllers/Cource.js
+++ server/controllers/Cource.js
@@ -80,7 +80,7 @@ exports.createCourse = async (req, res) => {
             {_id: category},
             {
                 $push:{
-                    course: newCourse._id,
+                    courses: newCourse._id,
                 },
             },
             {new : true}

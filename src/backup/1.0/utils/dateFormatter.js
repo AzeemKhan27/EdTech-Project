384c60ca690511cb58a8102e0385ad4e5f58443f
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-06-05 16:06:45 +0530

    Frontend : Remove Backup code from Remote Code.

--- src/backup/1.0/utils/dateFormatter.js
+++
@@ -1,7 +0,0 @@
-export const formattedDate = (data) => {
-    return new Date(data).toLocaleDateString().replace("en-US",{
-        month : "long",
-        day : "numeric",
-        year : "numeric"
-    } )
-}
